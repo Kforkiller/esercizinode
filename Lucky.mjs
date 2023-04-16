daf4d2e47f000c782e3e71454b0a7789a7635e75
Your Name <antonio.zappatore.br@gmail.com>
2023-04-16 20:10:53 +0200

    Async

---
+++ Lucky.mjs
@@ -0,0 +1,26 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+async function agetResults(player){
+    try{
+        for (player of ["Tina", "Jorge", "Julien"]){
+            const res = await luckyDraw(player)
+            console.log(res)
+        }
+
+    } catch (err){
+        console.error(err)
+    }
+}
+agetResults()
